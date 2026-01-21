118e735134749094f5df3b8dbeb6c4abbbcd92ee
imraj560 <raju560.webdev@gmail.com>
2026-01-21 17:27:30 -0500

    Secure our API with ArcJet

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ import morgan from 'morgan';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import authRoutes from '#routes/auth.routes.js';
+import securityMiddleware from '#middleware/security.middleware.js';
 
 const app = express();  
 
@@ -20,6 +21,8 @@ app.use(morgan('combined', {
     write: (message) => logger.info(message.trim()),
   },
 }));
+
+app.use(securityMiddleware);
 
 app.get('/', (req, res) => {
   logger.info('Root endpoint accessed');
