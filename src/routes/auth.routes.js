8c68a6d5847a85a825bf085496f5c8a1b6c27c68
imraj560 <raju560.webdev@gmail.com>
2026-01-21 16:45:04 -0500

    Signout Implemented

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,19 +1,13 @@
 import express from 'express';
-import { signup } from '../controllers/auth.controller.js';
+import { signup, signIn, signOut } from '../controllers/auth.controller.js';
 
 
 const router = express.Router();
 
 router.post('/sign-up', signup);
 
-router.post('/sign-in', (req, res) => {
-  // Handle sign-in logic here
-  res.status(200).send('POST /api/auth/sign-in endpoint hit');
-});
+router.post('/sign-in', signIn);
 
-router.post('/sign-out', (req, res) => {
-  // Handle sign-out logic here
-  res.status(200).send('POST /api/auth/sign-out endpoint hit');
-});
+router.post('/sign-out', signOut);
 
 export default router;
